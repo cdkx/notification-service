5cddd07f6e9d2ce43d8c6ca55acc94060b839551
Viktor Eremin <vektor451@gmail.com>
2025-07-02 04:37:03 +0300

    add cloudconfig.yaml & config-server

--- src/main/java/ru/eremin/notificationservice/NotificationServiceApplication.java
+++ src/main/java/ru/eremin/notificationservice/NotificationServiceApplication.java
@@ -3,17 +3,19 @@ package ru.eremin.notificationservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 import java.util.Date;
 
 
 @Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(NotificationServiceApplication.class, args);
-        log.info("notification service started on {}", new Date(context.getStartupDate()));
+        log.info("app NotificationService started on {}", new Date(context.getStartupDate()));
     }
 }
